384a3829f7df4aa1dfe69fe186fc9af77457c0a1
vesper3 <jeremyjnorwood@gmail.com>
2016-04-24 00:24:41 -0700

    Added files via upload

---
+++ TempPlatformerName/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+    public Player_Controller player;
+    public Vector2 focusAreaSize;
+
+    public float verticalOffset;
+    public float lookAheadX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
+
+    FocusArea focusArea;
+
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDircetionX;
+    float smoothVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
+
+    void Start()
+    {
+        focusArea = new FocusArea(player.collider.bounds, focusAreaSize);
+    }
+
+    void LateUpdate()
+    {
+        focusArea.Update(player.GetComponent<Collider2D>().bounds);
+        Vector2 focusPosition = focusArea.center + Vector2.up * verticalOffset;
+
+        if(focusArea.velocity.x != 0)
+        {
+            lookAheadDircetionX = Mathf.Sign(focusArea.velocity.x);
+            lookAheadStopped = false;
+            if (Mathf.Sign(player.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && player.playerInput.x != 0)
+            {
+                targetLookAheadX = lookAheadDircetionX * lookAheadX;
+            }
+            else
+            {
+                if (!lookAheadStopped)
+                {
+                    lookAheadStopped = true;
+                    targetLookAheadX = currentLookAheadX + (lookAheadDircetionX * lookAheadX - currentLookAheadX) / 4f;
+                }
+            }
+        }
+
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothVelocityX, lookSmoothTimeX);
+
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+        focusPosition += Vector2.right * currentLookAheadX;
+
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, .5f);
+        Gizmos.DrawCube(focusArea.center, focusAreaSize);
+    }
+
+    struct FocusArea
+    {
+        public Vector2 center;
+        public Vector2 velocity;
+        float left, right;
+        float top, bottom;
+
+        public FocusArea(Bounds targetBounds, Vector2 size)
+        {
+            left = targetBounds.center.x - size.x / 2;
+            right = targetBounds.center.x + size.x / 2;
+            bottom = targetBounds.min.y;
+            top = targetBounds.min.y + size.y;
+
+            velocity = Vector2.zero;
+            center = new Vector2((left + right) / 2, (top + bottom) / 2);
+        }
+
+        public void Update(Bounds targetBounds)
+        {
+            float shiftX = 0;
+            if(targetBounds.min.x < left)
+            {
+                shiftX = targetBounds.min.x - left;
+            }
+            else if(targetBounds.max.x > right)
+            {
+                shiftX = targetBounds.max.x - right;
+            }
+
+            left += shiftX;
+            right += shiftX;
+
+            float shiftY = 0;
+            if (targetBounds.min.y < bottom)
+            {
+                shiftY = targetBounds.min.y - bottom;
+            }
+            else if (targetBounds.max.y > top)
+            {
+                shiftY = targetBounds.max.y - top;
+            }
+
+            bottom += shiftY;
+            top += shiftY;
+
+            center = new Vector2((left + right) / 2, (top + bottom) / 2);
+            velocity = new Vector2(shiftX, shiftY);
+        }
+    }
+}
